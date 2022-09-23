a541f3a9db8fa88fff74ed5be472f59eaa5dc23b
slumpthug <gangguchi58@gmail.com>
2022-09-23 13:13:19 +0600

    added modal registration block

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css';
 import Content from './components/Content/Content';
 import Footer from './components/Footer/Footer';
-import Registration from './components/RegistrationModal/Registration';
 import Visit from './components/Visit/Visit';
 
 function App() {
   return (
     <div className="App">
-      {/* <Visit />
+      <Visit />
       <Content />
-      <Footer /> */}
-      <Registration/>
+      <Footer />
     </div>
   );
 }
